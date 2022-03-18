f93ae9afa84b4eb272892dcfaa493e155c7243ba
unknown <blackprince697@gmail.com>
2022-03-18 07:49:58 +0700

    bg-generator

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Hi, World")
+let body = document.querySelector('body')
+let button = document.querySelector('.colorchanger')
+let colorinput = document.querySelector('.color')
+
+colorinput.addEventListener('input', () => {
+    body.style.background = colorinput.value;
+})
+
+button.addEventListener('click', () => {
+    color = createColor();
+    body.style.background = color;
+    button.innerHTML = color;
+})
+
+const generateNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+const createColor = () => {
+    let color = "rgb("
+    for (let i = 0; i < 3; i++) {
+        newcolor = generateNumber(0, 255);
+        if (i != 2) {
+            color += newcolor + ", "
+        }
+        else {
+            color += newcolor + ")"
+        }
+    }
+    return color;
+}
